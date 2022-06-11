9eba5bf6d0182513af81e8b34ddcdd7f1793e378
Vencislav <vencislav67@hotmail.com>
2022-06-11 11:33:57 +0300

    Add project files.

---
+++ 01_Unique_Usernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> userNames = new HashSet<string>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string s = Console.ReadLine();
+                userNames.Add(s);
+            }
+
+            foreach(var name in userNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
