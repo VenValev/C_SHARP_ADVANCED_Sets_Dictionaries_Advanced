a7b07d17b8bf255b3bdbb0cb9a7a1efeb4c24f51
Vencislav <vencislav67@hotmail.com>
2022-06-11 12:06:09 +0300

    let us see

---
+++ 03_Periodic_table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Periodic_table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> pTable = new HashSet<string>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] chemicals = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+                for(int j = 0; j < chemicals.Length; j++)
+                {
+                    pTable.Add(chemicals[j]);
+                }
+            }
+
+            HashSet<string> periodicTable = pTable.OrderBy(x => x).ToHashSet();
+            Console.WriteLine(string.Join(" ", periodicTable));
+        }
+    }
+}
