42c1e6ffc535de56c8dc029e2d1f186b5c0284c8
Vencislav <vencislav67@hotmail.com>
2022-06-11 11:35:49 +0300

    Created 2

---
+++ 02_Sets_Of_Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_Sets_Of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
