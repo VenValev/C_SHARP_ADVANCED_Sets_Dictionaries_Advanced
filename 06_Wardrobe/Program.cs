8b5348f86f030602641056dd3f030fda44e054d7
Vencislav <vencislav67@hotmail.com>
2022-06-12 11:50:39 +0300

    made some code

--- 06_Wardrobe/Program.cs
+++ 06_Wardrobe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _06_Wardrobe
 {
@@ -6,7 +7,24 @@ namespace _06_Wardrobe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            SortedDictionary<string, Dictionary<string, int>> wardrobe = new SortedDictionary<string, Dictionary<string, int>>();
+
+            for(int i = 0; i < n; i++)
+            {
+                string text = Console.ReadLine();
+                string[] clothesArg = text.Split('->', StringSplitOptions.RemoveEmptyEntries);
+                string[] clothes = clothesArg[1].Split(',', StringSplitOptions.RemoveEmptyEntries);
+                if (!wardrobe.ContainsKey(clothesArg[0]))
+                {
+                    wardrobe.Add(clothesArg[0], new Dictionary<string, int>());
+                    for(int j = 0; j < clothes.Length; j++)
+                    {
+                        wardrobe[clothesArg[0]].Add(clothes[j], 0);
+                    }
+                }
+
+            }
         }
     }
 }
