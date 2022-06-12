4ec07e722ab067a9c1c71c52ef102333e3260cdb
Vencislav <vencislav67@hotmail.com>
2022-06-12 11:37:50 +0300

    5

---
+++ 05_Count_Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().Trim();
+            //string text1 = String.Concat(text.Where(c => !Char.IsWhiteSpace(c)));
+            SortedDictionary<char, int> symbolsCount = new SortedDictionary<char, int>();
+            char[] symbols = text.ToCharArray();
+
+            for(int i = 0; i < symbols.Length; i++)
+            {
+                if(!symbolsCount.ContainsKey(symbols[i]))
+                {
+                    symbolsCount.Add(symbols[i], 0);
+                }
+                symbolsCount[symbols[i]]++;
+            }
+
+            foreach(var symbol in symbolsCount.Keys)
+            {
+                Console.WriteLine($"{symbol}: {symbolsCount[symbol]} time/s");
+            }
+
+        }
+    }
+}
